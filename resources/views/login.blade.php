1d84101ad9382e0f543a5987c271154991bf70b2
IamKuro <saello2017@gmail.com>
2024-02-28 15:07:26 +0700

    Update login.blade.php
    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DOcument</title>
+    <title>Halaman Profil</title>
 </head>
 
 <body>
-<h1> Ini halaman login</h1>
+<h1>Ini adalah halaman login</h1>
 </body>
 
 </html>
